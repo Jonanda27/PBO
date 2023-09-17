b268b045c0e609004e520bb5d68349903452df61
Jonanda27 <123913405+Jonanda27@users.noreply.github.com>
2023-09-17 22:27:55 +0700

    Tugas4_Jonanda

---
+++ Tugas4/UpinIpin/src/main/java/com/mycompany/upinipin/UpinIpin.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.upinipin;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class UpinIpin {
+    public static void main(String[] args) {
+        //new Item digunakan untuk pemanggilan blok Item untuk menampilkan hasil
+        //"upin" akan dihiraukan dan tidak akan ditampilkan karena pada Item.java ipin merupakan private String
+        Item name = new Item("upin");
+    }    
+}
